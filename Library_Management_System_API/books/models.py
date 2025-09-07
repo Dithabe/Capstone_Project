539bd5bdaac5f776cdb89142b2306298835dd669
Dithabe <61557057+Dithabe@users.noreply.github.com>
2025-09-07 22:33:33 +0200

    Add files via upload

--- Library_Management_System_API/books/models.py
+++ Library_Management_System_API/books/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    isbn = models.CharField(max_length=13, unique=True)
+    published_date = models.DateField()
+    copies_available = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.title} by {self.author}"
+
+    class Meta:
+        ordering = ['published_date' ]
--- Library_Management_System_API/books/views.py
+++ Library_Management_System_API/books/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework import mixins, viewsets, permissions, filters
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import Book
+from .serializers import BookSerializer
 
-# Create your views here.
+class BookViewSet(
+    mixins.CreateModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
+    mixins.ListModelMixin,
+    viewsets.GenericViewSet
+):
+   
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    filterset_fields = ['title', 'author', 'isbn']
+    search_fields = ['title', 'author', 'isbn']
+
+    @action(detail=False, methods=['get', 'post'])
+    def available(self, request):
+        books = self.get_queryset().filter(copies_available__gt=0)
+        serializer = self.get_serializer(books, many=True)
+        return Response(serializer.data)
